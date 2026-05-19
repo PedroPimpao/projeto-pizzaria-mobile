4b1a1a19bf5e43c5d9c3f26305893ca9e101fe17
Pedro Pimpão <pedrohmpimpao@gmail.com>
2026-05-19 09:45:37 -0300

    feat: add input and button components

--- app/login.tsx
+++ app/login.tsx
@@ -1,20 +1,60 @@
+import Button from '@/components/Button';
+import Input from '@/components/Input';
 import Logo from '@/components/logo';
 import { colors, spacing } from '@/constants/theme';
+import { useState } from 'react';
 import {
   KeyboardAvoidingView,
+  Platform,
   ScrollView,
   StyleSheet,
-  Text,
   View,
 } from 'react-native';
+import { handleSignIn } from './utils/auth';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleEmailChange = (email: string) => {
+    setEmail(email);
+  };
+
+  const handlePasswordChange = (password: string) => {
+    setPassword(password);
+  };
+
+  const login = () => {
+    handleSignIn({ email, password });
+  };
   return (
-    <KeyboardAvoidingView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollContent}>
-        <Logo/>
-        <View>
-            
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.select({ ios: 'padding', android: 'height' })}
+    >
+      <ScrollView
+        contentContainerStyle={styles.scrollContent}
+        keyboardShouldPersistTaps="handled"
+        showsHorizontalScrollIndicator={false}
+      >
+        <Logo />
+        <View style={styles.formContainer}>
+          <Input
+            label="Email"
+            placeholder="Digite seu email"
+            keyboardType="email-address"
+            onChangeText={handleEmailChange}
+            value={email}
+          />
+          <Input
+            label="Senha"
+            placeholder="Digite sua senha"
+            secureTextEntry={true}
+            onChangeText={handlePasswordChange}
+            value={password}
+          />
+          <Button title="Entrar" onPress={login} loading={loading}/>
         </View>
       </ScrollView>
     </KeyboardAvoidingView>
@@ -30,6 +70,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flexGrow: 1,
     height: '100%',
-    paddingHorizontal: spacing.xl
+    paddingHorizontal: spacing.xl,
+  },
+  formContainer: {
+    gap: spacing.md,
   },
 });
