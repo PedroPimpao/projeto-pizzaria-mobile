15ed0f9b08c521bb424b11c3ab6dd3f6d41e8a17
Pedro Pimpão <pedrohmpimpao@gmail.com>
2026-05-22 14:33:51 -0300

    feat: add dashboard page

--- app/(authenticated)/_layout.tsx
+++ app/(authenticated)/_layout.tsx
@@ -1,3 +1,4 @@
+import { colors } from '@/constants/theme';
 import { useAuth } from '@/contexts/AuthContext';
 import { Stack, useRouter } from 'expo-router';
 import { useEffect } from 'react';
@@ -6,13 +7,6 @@ export default function AuthenticatedLayout() {
   const { loading, signed } = useAuth();
   const router = useRouter();
 
-  // useEffect(() => {
-  //   if(!loading && !signed){
-  //     router.replace('/login')
-  //   }
-  // }, [loading, signed])
-
-  // Versao aula
   useEffect(() => {
     if (!loading && !signed) {
       router.replace('/login');
@@ -23,10 +17,21 @@ export default function AuthenticatedLayout() {
     return null;
   }
 
-
   return (
-    <Stack>
-      <Stack.Screen name="dashboard" />
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: colors.background,
+        },
+        headerTintColor: colors.primary,
+        headerTitleStyle: {
+          fontWeight: '600',
+        },
+        headerShadowVisible: false,
+      }}
+    >
+      <Stack.Screen name="dashboard" options={{ headerShown: false }} />
     </Stack>
   );
 }
--- app/(authenticated)/dashboard.tsx
+++ app/(authenticated)/dashboard.tsx
@@ -1,23 +1,99 @@
+import Button from '@/components/Button';
+import Input from '@/components/Input';
+import Logo from '@/components/logo';
+import { borderRadius, colors, fontSize, spacing } from '@/constants/theme';
 import { useAuth } from '@/contexts/AuthContext';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function Dashboard() {
   const { signOut } = useAuth();
+  const insets = useSafeAreaInsets();
 
   return (
     <View style={styles.container}>
-      <Text>Dashboard Teste</Text>
-      <Button title="Sair" onPress={signOut} />
+      <StatusBar style="light" backgroundColor={colors.background} />
+      <KeyboardAvoidingView
+        style={styles.keyboardContainer}
+        behavior={Platform.select({ ios: 'padding', android: 'height' })}
+      >
+        <ScrollView
+          contentContainerStyle={styles.scrollContent}
+          keyboardShouldPersistTaps="handled"
+        >
+          <View style={[styles.header, { paddingTop: insets.top + 24 }]}>
+            <TouchableOpacity style={styles.signOutButton}>
+              <Text style={styles.signOutText}>Sair</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.content}>
+            <Logo />
+          </View>
+
+          <View style={styles.fieldContainer}>
+            <Text style={styles.title}>Novo pedido</Text>
+            <Input placeholder="Numero da mesa..." style={styles.input} />
+            <Button title="Abrir mesa" onPress={() => {}} />
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
-    alignItems: 'center',
     flex: 1,
-    color: '#FFF',
-    height: '100%',
+    backgroundColor: colors.background,
+  },
+  keyboardContainer: {
+    flex: 1,
+  },
+  scrollContent: {
+    flexGrow: 1,
+    width: '100%',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    paddingHorizontal: spacing.lg,
+    paddingBottom: spacing.sm,
+  },
+  signOutButton: {
+    backgroundColor: colors.red,
+    paddingVertical: spacing.sm,
+    paddingHorizontal: spacing.md,
+    borderRadius: borderRadius.md,
+  },
+  signOutText: {
+    color: colors.primary,
+    fontSize: fontSize.md,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: spacing.xl,
+  },
+  title: {
+    color: colors.primary,
+    fontSize: fontSize.xl,
+    fontWeight: '400',
+    marginHorizontal: 'auto'
+  },
+  input: {},
+  fieldContainer: {
+    flex: 1,
+    padding: spacing.lg,
+    flexDirection: 'column',
+    gap: spacing.lg,
   },
 });
